7b4f3ec3c4588adc3718b8074e5854ee8e02b5ac
Jesse Leung <jessetleung@aol.com>
2022-10-30 21:40:53 +0900

    [Update] Add types for api functions

--- src/discord_logger/external/api.py
+++ src/discord_logger/external/api.py
@@ -2,9 +2,9 @@ from src.discord_logger.internal.internal_api_receiver import (
     log_error_handler, plain_message_handler)
 
 
-def log_error(error_message, stack_trace=None):
+def log_error(error_message: str, stack_trace: str=None):
     log_error_handler(error_message, stack_trace)
 
 
-def plain_message(message):
+def plain_message(message: str):
     plain_message_handler(message)
--- src/discord_logger/internal/internal_api_receiver.py
+++ src/discord_logger/internal/internal_api_receiver.py
@@ -1,9 +1,9 @@
 from src.discord_logger import BotClient
 
 
-def log_error_handler(error_message, stack_trace=None):
+def log_error_handler(error_message: str, stack_trace: str=None):
     BotClient.send_error_message(error_message, stack_trace)
 
 
-def plain_message_handler(message):
+def plain_message_handler(message: str):
     BotClient.send_plain_message(message)
