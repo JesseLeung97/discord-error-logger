f29e04174c42d43aaf6901d2bdaefae84816efa2
Jesse Leung <jessetleung@aol.com>
2022-11-01 00:31:34 +0900

    [Update] Update __init__ file to expose api methods

--- discord_logger/__init__.py
+++ discord_logger/__init__.py
@@ -1,5 +1,6 @@
 from discord_logger.internal.bot_client import BotClient
 from discord_logger.internal.config import Config
+from discord_logger.external.api import (log_error, plain_message)
 
 if __name__ == "__main__":
     print("Starting up the bot client")
